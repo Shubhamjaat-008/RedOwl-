52f5e1e270af2b328baab135860f663f5824fb85
Shubhamjaat-008 <Shubha13@sheridancollege.ca>
2023-03-22 23:07:02 -0400

    Code commit

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -1,25 +1,36 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- */
-package ca.sheridancollege.project;
 
-/**
- * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
- * game. Students wishing to add to the code should remember to add themselves as a modifier.
- *
- * @author dancye
- */
-public abstract class Card {
-    //default modifier for child classes
+import java.util.ArrayList;
+import java.util.Collections;
 
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
-    @Override
-    public abstract String toString();
+public class Card {
 
+    public enum Suit {
+        HEARTS, DIAMONDS, CLUBS, SPADES
+    }
+
+    public enum Value {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    }
+
+public ArrayList<String> makeDeck()
+   {   
+    
+    Value[] values = Value.values();
+
+    Suit[] suit = Suit.values();
+
+    ArrayList<String> deck = new ArrayList<String>();
+
+    for (Suit s : suit) {
+        for (Value value : values) {
+            deck.add(value + " of " + s);
+        }
+    }
+
+    Collections.shuffle(deck);
+
+    return deck;
+
+    
+}
 }
--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -1,58 +1,132 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- */
-package ca.sheridancollege.project;
-
 import java.util.ArrayList;
+import java.util.Scanner;
 
-/**
- * The class that models your game. You should create a more specific child of this class and instantiate the methods
- * given.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
- */
-public abstract class Game {
+public class Game {
 
-    private final String name;//the title of the game
-    private ArrayList<Player> players;// the players of the game
+    private static Scanner scanner = new Scanner(System.in);
 
-    public Game(String name) {
-        this.name = name;
-        players = new ArrayList();
+    private static int currentPlayer = 1;
+
+    private static boolean isLying = false;
+
+    Player player = new Player();
+
+    private ArrayList<String> player1 = player.cardsToPlayerOne();
+    private ArrayList<String> player2 = player.cardsToPlayerTwo();
+
+    public Game() {
     }
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
+    public void playGame() {
+
+        while (player1.size() != 0 && player2.size() != 0) {
+            System.out.println("Player " + currentPlayer + "'s turn");
+            System.out.println("Your hand: " + getHand());
+
+            String cards;
+            String[] cardArray;
+
+            do {
+
+                cards = getCards();
+
+                cardArray = cards.split("  ");
+
+                if (cardArray.length != 2) {
+                    System.out.println("You didn't Enter a Space between Cards Enter again");
+
+                }
+            } while (cardArray.length != 2);
+
+            if (!isLying) {
+                System.out.println("Player " + currentPlayer + " is telling the truth. The cards are " + cards);
+            } else {
+                System.out.println("Player " + currentPlayer + " is lying. The cards are not " + cards);
+            }
+            boolean guess = getGuess();
+            if (guess == isLying) {
+                if (currentPlayer == 1) {
+                    System.out.println("Player 2 guessed correctly. Player 1 takes the cards back.");
+                    player1.addAll(cardArrayToList(cardArray));
+                } else {
+                    System.out.println("Player 1 guessed correctly. Player 2 takes the cards back.");
+                    player2.addAll(cardArrayToList(cardArray));
+                }
+                isLying = false;
+            } else {
+                if (currentPlayer == 1) {
+                    System.out.println("Player 2 guessed incorrectly. Player 2 takes the cards.");
+                    player2.addAll(cardArrayToList(cardArray));
+
+                    player1.remove(player1.indexOf(cardArray[0]));
+                    player1.remove(player1.indexOf(cardArray[1]));
+
+                } else {
+                    System.out.println("Player 1 guessed incorrectly. Player 1 takes the cards.");
+                    player1.addAll(cardArrayToList(cardArray));
+
+                    System.out.println(cardArray[0]);
+                    player2.remove(player2.indexOf(cardArray[0]));
+                    player2.remove(player2.indexOf(cardArray[1]));
+                }
+                isLying = true;
+            }
+            // Switch players
+            if (currentPlayer == 1) {
+                currentPlayer = 2;
+            } else {
+                currentPlayer = 1;
+            }
+        }
+        // Declare the winner
+        if (player1.size() == 0) {
+            System.out.println("Player 1 wins!");
+        } else {
+            System.out.println("Player 2 wins!");
+        }
     }
 
-    /**
-     * @return the players of this game
-     */
-    public ArrayList<Player> getPlayers() {
-        return players;
+    // Method to get the player's
+
+    public String getHand() {
+        String hand = "";
+        if (currentPlayer == 1) {
+            for (String card : player1) {
+                hand += card + " ";
+            }
+        } else {
+            for (String card : player2) {
+                hand += card + " ";
+            }
+        }
+        return hand;
     }
 
-    /**
-     * @param players the players of this game
-     */
-    public void setPlayers(ArrayList<Player> players) {
-        this.players = players;
+    // Method to get the cards from the player
+    public String getCards() {
+        System.out.println("Enter the cards you want to put down (separated by Two spaces): ");
+        String cards = scanner.nextLine();
+        return cards;
     }
 
-    /**
-     * Play the game. This might be one method or many method calls depending on your game.
-     */
-    public abstract void play();
+    // Method to get the other player's guess
+    public static boolean getGuess() {
+        System.out.println("Does the other player think you're lying? (y/n)");
+        String guess = scanner.nextLine();
+        if (guess.equals("y")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-    /**
-     * When the game is over, use this method to declare and display a winning player.
-     */
-    public abstract void declareWinner();
+    // Method to convert an array of cards to an ArrayList
+    public ArrayList<String> cardArrayToList(String[] cards) {
+        ArrayList<String> cardList = new ArrayList<String>();
+        for (String card : cards) {
+            cardList.add(card);
+        }
+        return cardList;
+    }
 
-}//end class
+}
